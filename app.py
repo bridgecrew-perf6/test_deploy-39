3a5dcb91788077dce3f2b78a0555776cbf7e59c5
aj <ajd1776.ad@gmail.com>
2022-01-27 16:03:28 +0000

    major dashboard changes

--- app.py
+++ app.py
@@ -2,22 +2,21 @@
 from flask import Flask
 
 def flask_app():
-    flaskapp = Flask(__name__)
-    #app.config.from_object('config.Config')
+    mainapp = Flask(__name__, instance_relative_config=False)
 
-    with flaskapp.app_context():
-        # Core flask app pages
+    with mainapp.app_context():
+        # Import parts of our core Flask app
         import routes
 
-        # Import Dash app
+        # Import Dash application
         from dashboard import init_dashboard
-        dashboard = init_dashboard(flaskapp)
+        mainapp = init_dashboard(mainapp)
 
-    return flaskapp
+        return mainapp
     
-app = flask_app()
-print("appdata: ", app)
+mainapp = flask_app()
+# print("appdata: ", app)
 
 # Run server
 if __name__ == "__main__":
-    app.run(debug=False)
+    mainapp.run(debug=False)
